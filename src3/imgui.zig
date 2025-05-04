d3aa4aecf0865d24de19940b98c74f5f4693e86e
Christian Burmeister <mail@cburmeister.me>
2025-05-04 11:11:20 +0200

    - Added a simple way to load gb files from command line and imgui.

--- src3/imgui.zig
+++ src3/imgui.zig
@@ -7,6 +7,7 @@ pub const State = struct {
     imgui_visible: bool = false,
     str_buff: [256]u8 = undefined,
     dump_dialog_open: bool = false,
+    gb_dialog_open: bool = false,
     current_dir: std.fs.Dir = undefined,
     // TODO: Need to find a good way for imgui (ui) to tell the system that the user did something.
     imgui_cb: ?*const fn ([]u8) void = null,
@@ -24,8 +25,13 @@ pub fn render(state: *State) void {
 
     if(imgui.igBeginMainMenuBar()) {
         if(imgui.igBeginMenu("File")) {
+            if(imgui.igMenuItem("Load GB")) {
+                state.gb_dialog_open = !state.gb_dialog_open;
+                state.dump_dialog_open = false;
+            }
             if(imgui.igMenuItem("Load Dump")) {
                 state.dump_dialog_open = !state.dump_dialog_open;
+                state.gb_dialog_open = false;
             }
             imgui.igEndMenu();
         }
@@ -36,49 +42,56 @@ pub fn render(state: *State) void {
 
         // TODO: Once we have a config file I can save the last folder that was opened and open the file dialog there.
         if(state.dump_dialog_open) {
-            const menu_height = 18;
-            imgui.igSetNextWindowPos(.{ .x = 0, .y = menu_height }, imgui.ImGuiCond_Once);
-            imgui.igSetNextWindowSize(.{ .x = def.window_width, .y = def.window_height - menu_height }, imgui.ImGuiCond_Once);
-            _ = imgui.igBegin("File Dialog", 0, imgui.ImGuiWindowFlags_NoResize | imgui.ImGuiWindowFlags_NoMove | imgui.ImGuiWindowFlags_NoCollapse);
-
-            if(imgui.igSelectable("..")) {
-                state.current_dir = state.current_dir.openDir("..", .{ .iterate = true }) catch unreachable;
-            }
-
-            var iter = state.current_dir.iterate();
-            while(iter.next() catch unreachable) |entry| {
-                switch(entry.kind) {
-                    .directory => {
-                        const title = std.fmt.bufPrintZ(&state.str_buff, "[Dir] {s}", .{ entry.name }) catch unreachable;
-                        if(imgui.igSelectable(@ptrCast(title))) {
-                            state.current_dir = state.current_dir.openDir(entry.name, .{ .iterate = true }) catch unreachable;
-                        }
-                    },
-                    .file => {
-                        var sequence = std.mem.split(u8, entry.name, ".");
-                        _ = sequence.next(); // skip filename
-                        if(sequence.next()) |extension| {
-                            if(!std.mem.eql(u8, extension, "dump")) {
-                                continue;
-                            }
-                        } else { // has no extension.
-                            continue;
-                        }
-                        const title = std.fmt.bufPrintZ(&state.str_buff, "[File] {s}", .{ entry.name }) catch unreachable;
-                        if(imgui.igSelectable(@ptrCast(title))) {
-                            const full_path = state.current_dir.realpath(entry.name, &state.str_buff) catch unreachable;
-                            if(state.imgui_cb) |callback| {
-                                callback(full_path);
-                            }
-                        }
-                    },
-                    else => {},
-                }
-            }
-
-            imgui.igEnd();
+            ShowFileDialogue(state, "dump");
+        }
+        if(state.gb_dialog_open) {
+            ShowFileDialogue(state, "gb");
         }
 
         imgui.igEndMainMenuBar();
     }
 } 
+
+pub fn ShowFileDialogue(state: *State, file_extension: []const u8) void {
+    const menu_height = 18;
+    imgui.igSetNextWindowPos(.{ .x = 0, .y = menu_height }, imgui.ImGuiCond_Once);
+    imgui.igSetNextWindowSize(.{ .x = def.window_width, .y = def.window_height - menu_height }, imgui.ImGuiCond_Once);
+    _ = imgui.igBegin("File Dialog", 0, imgui.ImGuiWindowFlags_NoResize | imgui.ImGuiWindowFlags_NoMove | imgui.ImGuiWindowFlags_NoCollapse);
+
+    if(imgui.igSelectable("..")) {
+        state.current_dir = state.current_dir.openDir("..", .{ .iterate = true }) catch unreachable;
+    }
+
+    var iter = state.current_dir.iterate();
+    while(iter.next() catch unreachable) |entry| {
+        switch(entry.kind) {
+            .directory => {
+                const title = std.fmt.bufPrintZ(&state.str_buff, "[Dir] {s}", .{ entry.name }) catch unreachable;
+                if(imgui.igSelectable(@ptrCast(title))) {
+                    state.current_dir = state.current_dir.openDir(entry.name, .{ .iterate = true }) catch unreachable;
+                }
+            },
+            .file => {
+                var sequence = std.mem.split(u8, entry.name, ".");
+                _ = sequence.next(); // skip filename
+                if(sequence.next()) |extension| {
+                    if(!std.mem.eql(u8, extension, file_extension)) {
+                        continue;
+                    }
+                } else { // has no extension.
+                    continue;
+                }
+                const title = std.fmt.bufPrintZ(&state.str_buff, "[File] {s}", .{ entry.name }) catch unreachable;
+                if(imgui.igSelectable(@ptrCast(title))) {
+                    const full_path = state.current_dir.realpath(entry.name, &state.str_buff) catch unreachable;
+                    if(state.imgui_cb) |callback| {
+                        callback(full_path);
+                    }
+                }
+            },
+            else => {},
+        }
+    }
+
+    imgui.igEnd();
+}
--- src3/mmu.zig
+++ src3/mmu.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const def = @import("defines.zig");
+const mem_map = @import("mem_map.zig");
 
 const MemoryRequest = struct {
     read: ?u16 = null,
@@ -33,9 +34,22 @@ pub fn init(_: *State) void {
 }
 
 pub fn loadDump(state: *State, path: []const u8) void {
-    const file = std.fs.openFileAbsolute(path, .{}) catch unreachable;
-    const len = file.readAll(&state.memory) catch unreachable;
-    std.debug.assert(len == state.memory.len);
+    // TODO: This should be handled in the cli itself. But this is easier for now :)
+    var file_extension: []const u8 = undefined;
+    var iter = std.mem.splitScalar(u8, path, '.');
+    while(iter.peek() != null) {
+        file_extension = iter.next().?;
+    }
+
+    if (std.mem.eql(u8, file_extension, "dump")) {
+        const file = std.fs.openFileAbsolute(path, .{}) catch unreachable;
+        const len = file.readAll(&state.memory) catch unreachable;
+        std.debug.assert(len == state.memory.len);
+    } else if (std.mem.eql(u8, file_extension, "gb")) {
+        const file = std.fs.openFileAbsolute(path, .{}) catch unreachable;
+        const len = file.readAll(state.memory[mem_map.rom_low..mem_map.rom_high]) catch unreachable;
+        std.debug.assert(len == mem_map.rom_high - mem_map.rom_low);
+    }
 }
 
 pub fn cycle(state: *State) void {
